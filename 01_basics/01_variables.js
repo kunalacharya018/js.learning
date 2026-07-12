bd7ea441f151f61b676752d450aebc12ecd720b2
Kunal Acharya <kunalacharya018@gmail.com>
2026-07-12 05:13:45 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 11223344
+
+let accountEmail = "kunnalacharya018@gmail.com"
+var accountPassword = "12345"
+
+accountCity = "jaipur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
