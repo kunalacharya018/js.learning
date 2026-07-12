2cdcf0ee6de1e0322d305b94302242d582069bec
Kunal Acharya <kunalacharya018@gmail.com>
2026-07-12 10:22:11 +0000

    conversion of dataTypes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,53 +1,71 @@
-let score = "33"
+// let score = "33"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
-
-
-score = null
-
-console.log(typeof score);
-console.log(typeof (score));
-
-valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
-score = undefined
+// score = null
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
-
-
-score = true
-
-console.log(typeof score);
-console.log(typeof (score));
-
-valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
+// score = undefined
 
-/*
-"33"  -> 33
-"33abc" -> NaN
-true -> 1 ; false -> 0
+// console.log(typeof score);
+// console.log(typeof (score));
 
-*/
+// valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-let isloggedIn = "kunnal"
 
-let booleanIsloggedIn = Boolean(isloggedIn)
+// score = true
 
-console.log(booleanIsloggedIn);
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// /*
+// "33"  -> 33
+// "33abc" -> NaN
+// true -> 1 ; false -> 0
+
+// */
+
+// let isloggedIn = "kunnal"
+
+// let booleanIsloggedIn = Boolean(isloggedIn)
+
+// console.log(booleanIsloggedIn);
+
+
+
+
+
+//*******************  Operations *******************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/3);
+// console.log(2%2);
+// console.log(2**3);
