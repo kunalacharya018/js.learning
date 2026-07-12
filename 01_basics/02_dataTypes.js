d4eacadcd960018825a26e059e7bc96ba6adc670
Kunal Acharya <kunalacharya018@gmail.com>
2026-07-12 05:14:10 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+let name = "kunnal"
+let age = 19
+let isloggedIn = false
+let state;
+
+/*
+number => 2 to the power 53
+bigint
+string => ""
+boolean = true/false
+null => standalone value
+undefined => 
+symbol =>unique
+
+object
+*/
+
+console.log(typeof undefined);
